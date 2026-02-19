2745cddd17b6aac0913c606fc4f9dfdd43b87cae
Omar Carrillo <ocarrillo@MacBook-Air-de-Omar.local>
2026-02-19 14:57:22 -0600

    chore: limpia workflows duplicados y red EKS

--- nginx-plus-one-eks/terraform/main.tf
+++ nginx-plus-one-eks/terraform/main.tf
@@ -32,11 +32,31 @@ resource "aws_vpc" "eks" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_internet_gateway" "eks" {
+  vpc_id = aws_vpc.eks.id
+}
+
+resource "aws_route_table" "eks_public" {
+  vpc_id = aws_vpc.eks.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks.id
+  }
+}
+
 resource "aws_subnet" "eks" {
   count             = 2
   vpc_id            = aws_vpc.eks.id
   cidr_block        = cidrsubnet(aws_vpc.eks.cidr_block, 8, count.index)
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table_association" "eks_public" {
+  count          = 2
+  subnet_id      = aws_subnet.eks[count.index].id
+  route_table_id = aws_route_table.eks_public.id
 }
 
 data "aws_availability_zones" "available" {}
