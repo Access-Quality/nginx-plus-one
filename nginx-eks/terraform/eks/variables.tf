4db7bba7fe6d663e481d744b7f1856fcd9f09b6c
Omar Carrillo <ocarrillo@MacBook-Air-de-Omar.local>
2026-02-20 15:40:31 -0600

    feat(nginx-eks): upgrade Kubernetes to 1.33

--- nginx-eks/terraform/eks/variables.tf
+++ nginx-eks/terraform/eks/variables.tf
@@ -12,7 +12,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version for the EKS control plane."
   type        = string
-  default     = "1.32"
+  default     = "1.33"
 }
 
 variable "name_prefix" {
