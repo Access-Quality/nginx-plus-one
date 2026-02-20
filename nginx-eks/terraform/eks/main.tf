c3dc2360139851c6c5f7c5ae460e8c6b991ed406
Omar Carrillo <ocarrillo@MacBook-Air-de-Omar.local>
2026-02-20 14:05:52 -0600

    feat(nginx-eks): remove NAT Gateway, move nodes to public subnets (test env)

--- nginx-eks/terraform/eks/main.tf
+++ nginx-eks/terraform/eks/main.tf
@@ -17,17 +17,14 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  enable_nat_gateway   = false
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   # Tags required by the AWS Load Balancer Controller / NGINX Ingress
   public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/role/elb"            = 1
+    "kubernetes.io/role/internal-elb"   = 1
   }
 }
 
@@ -40,7 +37,7 @@ module "eks" {
   cluster_version = var.cluster_version
 
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  subnet_ids = module.vpc.public_subnets
 
   # Public access so kubectl works from GitHub Actions
   cluster_endpoint_public_access = true
